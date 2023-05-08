4e8ce2d175bc86fd204e19e4f34ca02c60461783
olisabelema <olisabelema@gmail.com>
2023-05-08 15:37:19 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(" ".join(str.split()[5:9] + [str.split()[11]]) + " " + str.split()[0])
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
