83de7f9bc60fe0075e4d26f16252af9d69b494ef
olisabelema <olisabelema@gmail.com>
2023-05-09 16:13:40 -0700

    update

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(s, n):
     # Create a new empty string to hold the result
     result = ""
-    
+
     # Loop over the characters in the original string
     for i in range(len(s)):
         # Skip the character at index n
@@ -10,6 +10,6 @@ def remove_char_at(s, n):
             continue
         # Add all other characters to the result string
         result += s[i]
-    
+
     # Return the result string
-    return (result);
+    return (result)
