3057d59103e18bc0cbbbfc58e844d477c5363123
olisabelema <olisabelema@gmail.com>
2023-05-11 15:04:06 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the math result of 10 and 5."""
+    """Prints the math result of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
