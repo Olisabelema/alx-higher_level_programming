7f326079f5818ef236cb962ac7fe8862fe1c66ff
olisabelema <olisabelema@gmail.com>
2023-05-11 14:11:36 -0700

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":    
+    from add_0 import add
 
-def main():
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
-
-if __name__ == "__main__":
-    main()
+    print(f"{a} + {b} = {add(a, b)}")
