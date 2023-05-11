2f19b20f3436b6251fc7fe18963f85dcb9284b56
olisabelema <olisabelema@gmail.com>
2023-05-11 16:36:40 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 import sys
 
 if __name__ == "__main__":
-    # Calculate the sum of all arguments
-    arguments = sys.argv[1:]  # Get all arguments except the first one (which is the script name)
+    """ Calculate the sum of all arguments."""
+    arguments = sys.argv[1:]
     total = sum([int(arg) for arg in arguments])
 
-    # Print the total sum
+    """ Print the total sum."""
     print("{}".format(total))
