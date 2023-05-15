f813de8b3064edee818090abe2339307750fb845
olisabelema <olisabelema@gmail.com>
2023-05-15 10:59:10 -0700

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/bin/usr/python3
 
 def new_in_list(my_list, idx, element):
-    """Replace an element in a list at a specific position without modifying the original list."""
+    """Return a new list with an element replaced at a specific position."""
     if idx < 0 or idx >= len(my_list):
-        return (my_list[:])
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return (new_list)
+        return (my_list.copy())
+
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return (new_list)
