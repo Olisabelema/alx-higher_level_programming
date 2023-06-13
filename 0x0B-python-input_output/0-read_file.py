b2a43844b8883041cf1c8605b20d861b652bc6a8
olisabelema <olisabelema@gmail.com>
2023-06-13 08:49:21 -0700

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,7 @@ a function that reads a text file (UTF8) and prints it to stdout
 
 def read_file(filename=""):
     """read file utf8 con with """
-    with open(filename, encoding='UTF-8') as file:
-        for line in file:
+    with open(filename, encoding='UTF-8') as f:
+        for line in f:
             print(line, end='')
 
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,6 +12,6 @@ def append_write(filename="", text=""):
         Exception: when the file can be opened
     """
 
-    with open(filename, 'a', encoding="utf-8") as file:
-        return file.write(text)
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
 
