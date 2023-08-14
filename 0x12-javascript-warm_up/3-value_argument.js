7f58323ae67d45812f0619b925a9a070824d3b4c
olisabelema <olisabelema@gmail.com>
2023-08-14 21:41:22 +0000

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,10 +2,12 @@
 
 /*
  * argv[0] = path where the node.js program is located
- * argv[1] = path to the currenly executed file
- * argv[2] = the first argument provided when running the program
+ * argv[1] = path to thecurrently executed file
+ * argv[2] = the first argument passed to the program
  */
 
-const argument = process.argv[2];
-console.log(argument ? argument : 'No argument');
-
+if (!process.argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
