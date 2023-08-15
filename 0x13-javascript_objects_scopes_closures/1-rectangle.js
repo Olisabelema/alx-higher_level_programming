bf94a819930486e6b3963350cf1a34ced0e6c474
olisabelema <olisabelema@gmail.com>
2023-08-15 13:44:24 +0000

    update

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 
 /*
-Write a class Rectangle that defines a rectangle:
+Define a Rectangle class representing a rectangle shape.
 Instance attributes: width, height;
-Constructor arguments/parameter: w, h;
+Constructor arguments: w (width), h (height);
 */
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
   }
-};
+}
+
+module.exports = Rectangle;
+
