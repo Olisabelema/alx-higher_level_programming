cfb47f17c449aca7eb8cbe3a19d7939875acfc0f
olisabelema <olisabelema@gmail.com>
2023-08-19 12:26:07 -0700

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,21 +6,27 @@ Script that lists all states from the database hbtn_0e_0_usa.
 import sys
 import MySQLdb
 
+
 def main():
     """
     Main function that connects to the MySQL server and lists states.
     """
     if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+        print("""Usage: ./0-select_states.py
+            [mysql_username]
+            [mysql_password]
+            [database_name]""")
         return
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     try:
         # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306, user=username, passwd=password, db=database)
         cursor = db.cursor()
 
         # Execute the query to retrieve states
@@ -40,6 +46,6 @@ def main():
         print("Error:", e)
         sys.exit(1)
 
+
 if __name__ == "__main__":
     main()
-
