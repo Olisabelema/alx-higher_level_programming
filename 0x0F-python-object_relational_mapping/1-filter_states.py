89b4f107492e2729bf73aea3e1cfebe93740151f
olisabelema <olisabelema@gmail.com>
2023-08-19 13:36:50 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -35,7 +35,8 @@ def main():
         # Fetch all rows and display results
         states = cursor.fetchall()
         for state in states:
-            print(state)
+            if state[1][0] == "N":
+                print(state)
 
         # Close the cursor and database connection
         cursor.close()
