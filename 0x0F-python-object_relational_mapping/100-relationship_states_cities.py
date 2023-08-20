611182734f5de244f4350e67d632c2921a473277
olisabelema <olisabelema@gmail.com>
2023-08-20 14:35:49 -0700

    update

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python3
-
 """
-This module creates the State California with the City San Francisco
-from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
+Creates the State "California" with the City "San Francisco"
+from the database hbtn_0e_100_usa
 """
 
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
-if __name__ == '__main__':
 
-    usr = argv[1]
-    pswd = argv[2]
-    db = argv[3]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        usr, pswd, db), pool_pre_ping=True)
+    # Create a connection to the database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name
+        )
+    )
 
-    Base.metadata.create_all(engine)
-
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    """Create new state and city"""
-    new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    """Associate the new city to the newly created state"""
+    # Create a new State "California" and City "San Francisco"
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+
+    # Add the City to the State's cities relationship
     new_state.cities.append(new_city)
 
+    # Add the State and City objects to the session
     session.add(new_state)
     session.add(new_city)
 
+    # Commit the changes
     session.commit()
-    session.close()
