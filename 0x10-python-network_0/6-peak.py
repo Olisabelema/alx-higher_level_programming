e922bb28ed5dfd57bdcb62fffb0f862a71c5375c
olisabelema <olisabelema@gmail.com>
2023-08-31 12:37:33 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains function that finds the peak in a list of unsorted
-list of integers.
-"""
+"""Module to find the peak in a list of integers"""
 
 
-def find_peak(list_of_integers):
-    """Return number that is > the numbers both at its left and right side"""
-
-    if not list_of_integers:
-        return None
-
-    new_list = list_of_integers[:]
-    start, end = 0, len(new_list) - 1
-
-    if new_list[start] > new_list[start+1]:
-        return new_list[start]
-    if new_list[end] > new_list[end-1]:
-        return new_list[end]
-
-    mid = (start+end)//2
-    if new_list[mid-1] < new_list[mid] and new_list[mid+1] < new_list[mid]:
-        return new_list[mid]
-    if new_list[mid] < new_list[mid-1]:
-        return find_peak(new_list[start:mid+1])
-    elif new_list[mid] < new_list[mid+1]:
-        return find_peak(new_list[mid:end+1])
+def find_peak(nums):
+    """find_peak function definition"""
+    size = len(nums)
+    if (size == 0):
+        return
+    mid_index = size // 2
+    if (mid_index == size - 1 or nums[mid_index] >= nums[mid_index + 1]) \
+            and (nums[mid_index] >= nums[mid_index - 1] or mid_index == 0):
+        return nums[mid_index]
+    if (mid_index != size - 1) and nums[mid_index + 1] > nums[mid_index]:
+        return find_peak(nums[mid_index+1:])
     else:
-        return new_list[start]
+        return find_peak(nums[:mid_index])
