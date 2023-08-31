131cc75dce83afa567e620fbf5bc9ce9512f428a
olisabelema <olisabelema@gmail.com>
2023-08-31 12:23:53 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL provided as an argument and displays only the status code of the response.
-
-response=$(curl -sw "%{http_code}" -o /dev/null "$1")
-echo "$response"
+# Bash script that sends a request to a URL passed as an argument
+curl -o /dev/null -w '%{http_code}' -sIL "$1"
